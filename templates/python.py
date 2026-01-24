689a312e3dc6e1c4bd94d42a9bdd9c73b7b5089e
brrond <danya17072@gmail.com>
2026-01-24 20:38:53 +0100

    Add python template

---
+++ templates/python.py
@@ -0,0 +1,41 @@
+"""
+My Python template for competitive programming.
+Includes some basic wrapper logic easy debugging.
+"""
+
+
+class CPSolver:
+    """Class to handle input for debugging."""
+
+    def __init__(self, debug: bool = False):
+        self.debug = debug
+        self.file = None
+
+    def __call__(self):
+        """Caller method for the class."""
+
+        if self.debug:
+            self.file = open("../input.txt", "r", encoding="utf-8")
+        self.solve()
+        if self.debug and self.file is not None:
+            self.file.close()
+
+    def get_string(self) -> str:
+        """Get the string either from input file or from user input."""
+
+        if self.debug and self.file is not None:
+            return self.file.readline()
+        return input()
+
+    def solve(self):
+        """Solution goes here."""
+
+
+def main():
+    """Main method."""
+
+    CPSolver()()
+
+
+if __name__ == "__main__":
+    main()
