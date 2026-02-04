69d4b2f1ba5e1f73b33f94d05d2848b4f923c0e4
brrond <danya17072@gmail.com>
2026-02-04 18:16:48 +0100

    Fix kattis/bigboxes.py O(n log n)

--- kattis/bigboxes.py
+++ kattis/bigboxes.py
@@ -3,7 +3,6 @@ Link: https://open.kattis.com/problems/bigboxes?tab=metadata
 """
 
 import sys
-from functools import cache
 
 sys.setrecursionlimit(200_000)
 
@@ -18,66 +17,46 @@ else:
 n, k = list(map(int, lines[0].split()))
 weights = list(map(int, lines[1].split()))
 
-# calc sum array
-sums = []
-last_sum = 0
-for el in weights:
-    last_sum += el
-    sums.append(last_sum)
+# The question is, what is the minimum possible maximum
+# sum of splits.
+# It's possible to make an assumption that our solution is x.
+# The search region for: x in [max(weights); sum(weights)]
+# The question is, is it possible to create such a split,
+# that all parts: sum(part) <= x.
 
 
-@cache
-def get_sum(l, r):
+def can_split(x):
     """
-    Returns the sum on [l, r] interval (inclusive both ends).
-
-    Complexity: O(1).
+    O(n)
     """
 
-    return sums[r] - sums[l] + weights[l]
+    current_k = 1
+    curr_sum = 0
+
+    for el in weights:
+        if curr_sum + el > x:
+            current_k += 1
+            curr_sum = el
+        else:
+            curr_sum += el
+
+        if current_k > k:
+            break
+
+    return current_k <= k
 
 
-@cache
-def solve(l=0, r=n - 1, current_k=k) -> int:
-    """
-    Returns optimal solution for the problem:
-    Minimum possible maximal sum of the elements in one group.
+# Worst case is when max(weights) = 1 and sum(weights) = 100_000
+# The complexity of the search is therefore O(log n)
+l = max(weights)
+r = sum(weights)
+while l < r:
+    m = (l + r) // 2
 
-    l, r - left and right position in array,
-    current_k - current number of boxes to put items into.
+    if can_split(m):
+        r = m
+    else:
+        l = m + 1
+print(l)
 
-    Complexity: O(k*n!)
-    """
-
-    # impossible
-    if l > r:
-        return 0
-
-    # there can be one sum only
-    if current_k == 1:
-        return get_sum(l, r)
-
-    # "bite" first part of the array
-    # on split_position location.
-    # After that, current_k - 1 boxes are possible
-    best_max_sum = None
-
-    # [l, r)
-    for split_position in range(l, r + 1):
-
-        # calc sum of the current box
-        curr_sum = get_sum(l, split_position)
-
-        # calculate the alternative solution
-        alternative = solve(split_position + 1, r, current_k - 1)
-
-        # calculate potential best solution in this case
-        potential_best = max(alternative, curr_sum)
-
-        if best_max_sum is None or potential_best < best_max_sum:
-            best_max_sum = potential_best
-
-    return best_max_sum
-
-
-print(solve())
+# Final complexity: O(n log n)
