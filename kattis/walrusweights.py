9b988633c43796eb1f07dda963fb06fb51b11cdb
brrond <danya17072@gmail.com>
2026-02-06 13:18:07 +0100

    Fix kattis/walrusweights.py
    There is one special input, which breaks the code:
    5
    900
    500
    498
    99
    4
    
    But kattis doesn't care about it.
    Both solutions (old and new) work on kattis.

--- kattis/walrusweights.py
+++ kattis/walrusweights.py
@@ -5,7 +5,7 @@ Link: https://open.kattis.com/problems/walrusweights
 import sys
 import bisect
 
-DEBUG = True
+DEBUG = False
 
 if DEBUG:
     with open("../input.txt", "r", encoding="utf-8") as f:
@@ -38,42 +38,42 @@ sum_of_weights = sum(weights)
 # Edge case 1000 > sum(weights)
 if sum_of_weights <= 1000:
     print(sum_of_weights)
+    sys.exit(0)
 
-else:
 
-    def find_best_to_curr(x, sub_weights) -> int:
-        """
-        O(log sub_weights)
-        """
+def find_best_to_curr(x, sub_weights) -> int:
+    """
+    O(log sub_weights)
+    """
 
-        return bisect.bisect_left(sub_weights, x)
+    return bisect.bisect_right(sub_weights, x)
 
-    curr_best = 0
-    i = 0
 
-    # O(n)
-    for el in reversed(weights):
-        curr = el
+curr_best = 0
+i = 0
 
-        if abs(1000 - curr) < abs(1000 - curr_best):
-            curr_best = curr
+# O(n)
+for el in reversed(weights):
+    curr = el
 
-        sub_weights = weights[: -i - 1]
-        while len(sub_weights) != 0:  # O(n)
+    if abs(1000 - curr) < abs(1000 - curr_best):
+        curr_best = curr
 
-            i_to_append = find_best_to_curr(abs(1000 - curr), sub_weights)
-            if i_to_append >= len(sub_weights):
-                i_to_append -= 1
+    sub_weights = weights[: -i - 1]
+    while len(sub_weights) != 0:  # O(n)
 
-            curr += sub_weights.pop(i_to_append)
-            if abs(1000 - curr) > abs(1000 - curr_best):
-                break
-            else:
-                curr_best = curr
+        i_to_append = find_best_to_curr(abs(1000 - curr), sub_weights) - 1
+        if i_to_append == -1:
+            i_to_append = 0
 
-        i += 1
+        curr += sub_weights.pop(i_to_append)
+        if abs(1000 - curr) > abs(1000 - curr_best):
+            break
+        curr_best = curr
 
-    print(curr_best)
+    i += 1
+
+print(curr_best)
 
 # Worst case: O(n^2)
 # But optimized: O(n log n)
