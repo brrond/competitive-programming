6b9f267a8815dded9c00493b547d929e31e76141
brrond <danya17072@gmail.com>
2026-02-16 21:21:56 +0100

    Update python template to include output.txt

--- templates/python.py
+++ templates/python.py
@@ -1,6 +1,5 @@
 """
-My Python template for competitive programming.
-Includes some basic wrapper logic easy debugging.
+Link:
 """
 
 import sys
@@ -11,22 +10,29 @@ class CPSolver:
 
     def __init__(self, debug: bool = False):
         self.debug = debug
-        self.file = None
+        self.input_file = None
+        self.output_file = None
 
     def __call__(self):
         """Caller method for the class."""
 
         if self.debug:
-            self.file = open("../input.txt", "r", encoding="utf-8")
+            self.input_file = open("../input.txt", "r", encoding="utf-8")
+            self.output_file = open("../output.txt", "w", encoding="utf-8")
+
         self.solve()
-        if self.debug and self.file is not None:
-            self.file.close()
+
+        if self.debug:
+            if self.input_file is not None:
+                self.input_file.close()
+            if self.output_file is not None:
+                self.output_file.close()
 
     def get_string(self) -> str:
         """Get the string either from input file or from user input."""
 
-        if self.debug and self.file is not None:
-            return self.file.readline()
+        if self.debug and self.input_file is not None:
+            return self.input_file.readline()
         return sys.stdin.readline()
 
     def get_ints(self) -> list[int]:
@@ -38,6 +44,14 @@ class CPSolver:
         """Returns an integer from the input."""
 
         return int(self.get_string())
+
+    def put_string(self, string: str) -> None:
+        """Prints the string into the output."""
+
+        if self.debug and self.output_file is not None:
+            self.output_file.write(string + "\n")
+        else:
+            sys.stdout.write(string + "\n")
 
     def solve(self):
         """Solution goes here."""
